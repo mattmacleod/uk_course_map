aca32eb891fa0df2b9d61ebdfb516e005034008b
Matthew MacLeod <matt@matt-m.co.uk>
2013-04-13 12:23:57 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UkCourseMap::Application.config.secret_token = 'e6c74117be490b76bd5961512859117d465502a4a3450ed143cbc644930efb8d57b05eb265bee9b021300871163ebd0bb4d6912fe09311b0dccdb4a48d16c49c'
