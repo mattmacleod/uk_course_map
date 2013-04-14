bad04579382d6f80d00f9371bee09d9032acfae5
Matthew MacLeod <matt@matt-m.co.uk>
2013-04-14 06:08:25 +0100

    Added spinner to search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,12 +2,17 @@ ukcoursemap.search = {
 
 	setup: function(){
 		$("#search_button").click(function(){
+
+			$("#search_button").after("<div id='search_spinner'/>").fadeIn();
+
+			$("#search_spinner").fadeIn();
+			
 			$.ajax({
 				url: "/filter_results.json",
 				type: "GET",
 				data: $("#search_form").serialize(),
 				success: function(data){
-
+					$("#search_spinner").fadeOut(function(){ $(this).remove(); });
 				}
 			});
 			return false;
