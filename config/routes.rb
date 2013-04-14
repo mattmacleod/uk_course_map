87d4647314e719013082a76f42f75c0e7313cae2
Matthew MacLeod <matt@matt-m.co.uk>
2013-04-14 01:21:23 +0100

    Tidied up

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ UkCourseMap::Application.routes.draw do
   root :to => "search#index"
 
   get "courses.json" => "search#getcourses"
-
-  get "level1" => "search#level1"
+  get "level1"       => "search#level1"
 
   get "all_data.json" => "search#all_data"
-  get "jobs.json" => "search#jobs"
+  get "jobs.json"     => "search#jobs"
 
 end
